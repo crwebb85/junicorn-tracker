225ddffb6e1959f9ab198861c6cc1fc59aa7e00b
crwebb85 <51029315+crwebb85@users.noreply.github.com>
2022-02-17 13:01:02 -0500

    Added environment variables (#10)
    * Added environment variables

--- packages/infrastructure/src/junicorn-tracker-stack.ts
+++ packages/infrastructure/src/junicorn-tracker-stack.ts
@@ -7,15 +7,37 @@ import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
 import { Construct } from 'constructs';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 
+interface JunicornTrackerLambdaConfig {
+  webdavDomainName: string;
+  webdavUsername: string;
+  webdavPassword: string;
+  webdavUploadPath: string;
+  instagramUsername: string;
+  instagramPassword: string;
+}
 export class JunicornTrackerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const env: JunicornTrackerLambdaConfig = {
+      webdavDomainName: process.env['WEBDAV_DOMAIN_NAME'] ?? '',
+      webdavUsername: process.env['WEBDAV_USERNAME'] ?? '',
+      webdavPassword: process.env['WEBDAV_PASSWORD'] ?? '',
+      webdavUploadPath: process.env['WEBDAV_UPLOAD_PATH'] ?? '',
+      instagramUsername: process.env['INSTAGRAM_USERNAME'] ?? '',
+      instagramPassword: process.env['INSTAGRAM_PASSWORD'] ?? '',
+    };
 
     const junicornTrackerLambda = new Function(this, 'InstagramTrackerLambda', {
       code: Code.fromAsset(path.join(__dirname, 'lambda')),
       handler: 'index.handler',
       runtime: Runtime.PYTHON_3_9,
       architecture: Architecture.ARM_64,
+      environment: {
+        region: Stack.of(this).region,
+        availabilityZones: JSON.stringify(Stack.of(this).availabilityZones),
+        ...env,
+      },
     });
 
     const cronRule = new Rule(this, 'Scheduler', {
