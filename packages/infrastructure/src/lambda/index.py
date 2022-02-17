7fdcf6a47e8780dede0ded16c38b017621f6318f
crwebb85 <crwebb85@gmail.com>
2022-02-17 12:57:25 -0500

    Added environment variables

--- packages/infrastructure/src/lambda/index.py
+++ packages/infrastructure/src/lambda/index.py
@@ -1,2 +1,11 @@
+import os
+
 def handler(event, context):
     print("Hello World")
+
+    print(os.environ.get('webdavDomainName'))
+    print(os.environ.get('webdavUsername'))
+    print(os.environ.get('webdavPassword'))
+    print(os.environ.get('webdavUploadPath'))
+    print(os.environ.get('instagramUsername'))
+    print(os.environ.get('instagramPassword'))
